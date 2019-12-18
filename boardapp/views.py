4b19da06b918903bebe04ffc8c1365993fa02d89
gentearanea <gentearanea@gmail.com>
2019-12-18 22:53:37 +0900

    add: login機能

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect
 
 # Create your views here.
 def signupfunc(request):
@@ -16,3 +18,17 @@ def signupfunc(request):
             return render(request, 'signup.html', {'some':100})
 
     return render(request, 'signup.html', {'some':100})
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username2, password=password2)
+    
+        if user is not None:
+            login(request, user)
+            return redirect('signup')
+        else:
+            return  redirect('login')
+    return render(request, 'login.html')
